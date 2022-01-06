f81aae14ad2e7e002b449d0c9810b6d09a23f2dc
Xianzheng <mark.ffang@gmail.com>
2022-01-06 16:01:58 +0800

    try connect to django

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import Header from "./components/Header";
-import { useState } from "react";
-
+import axios from "axios";
+import { useState, useEffect } from "react";
+import NewForm from "./components/Form";
 import React from "react";
-
+import { v1 as uuid } from "uuid";
 import YoutubeEmbed from "./components/YoutubeEmbed";
 
 export default function App() {
+  const [songs, setSongs] = useState([
+    // { title: "almost home", id: 1 },
+    // { title: "almost there", id: 2 },
+    // { title: "almost that", id: 3 },
+  ]);
+
+  const addSong = (data) => {
+    console.log(data);
+    setSongs([
+      ...songs,
+      { id: data.id, title: data.title, completed: data.completed },
+    ]);
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/task-list/")
+      .then((res) => {
+        setSongs(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>Youtube Embed</h1>
       <YoutubeEmbed embedId="NdwFmtLgDVU" />
+      <div>
+        {songs.map((song) => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
+      </div>
+      <NewForm addSong={addSong} />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
+const NewForm = ({ addSong }) => {
+  const [url, setURL] = useState("");
+
+  const handle = (e) => {
+    e.preventDefault();
+    const address = "http://127.0.0.1:8000/api/task-create/";
+    const data = { title: url, completed: false };
+    axios
+      .post(address, data)
+      .then((res) => {
+        addSong(res.data);
+      })
+      .catch((err) => err);
+  };
+  return (
+    <form onSubmit={handle}>
+      <label>add youtube url</label>
+      <input
+        type="text"
+        value={url}
+        required
+        onChange={(e) => {
+          setURL(e.target.value);
+        }}
+      ></input>
+      <input type="submit" value="add URL" />
+    </form>
+  );
+};
+
+export default NewForm;
