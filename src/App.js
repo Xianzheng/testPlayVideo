b19d9258b2269818dea158c2b32b205cd24a4091
Xianzheng <mark.ffang@gmail.com>
2022-01-07 16:47:15 +0800

    add delete function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Header from "./components/Header";
+
 import axios from "axios";
 import { useState, useEffect } from "react";
 import NewForm from "./components/Form";
 import React from "react";
-import { v1 as uuid } from "uuid";
+
 import YoutubeEmbed from "./components/YoutubeEmbed";
 
+//const host = "http://127.0.0.1:8000"
+const host = "https://pickend.herokuapp.com"
 export default function App() {
   const [songs, setSongs] = useState([
     // { title: "almost home", id: 1 },
@@ -20,10 +22,10 @@ export default function App() {
       { id: data.id, title: data.title, completed: data.completed },
     ]);
   };
-
+  // "https://pickend.herokuapp.com/api/task-list/
   useEffect(() => {
     axios
-      .get("https://pickend.herokuapp.com/api/task-list/")
+      .get(host.concat("/api/task-list/"))
       .then((res) => {
         setSongs(res.data);
       })
@@ -32,13 +34,31 @@ export default function App() {
       });
   }, []);
 
+  const Delete = (id) =>{
+    const address = host.concat("/api/task-delete/"+id+"/")
+    axios
+      .delete(address)
+      .then((res)=>{
+        setSongs(songs.filter(res => res.id !== id));
+      })
+      .catch((err)=>{
+        console.log(err)
+      });
+  }
+
   return (
     <div className="App">
       <h1>Youtube Embed</h1>
-      <YoutubeEmbed embedId="NdwFmtLgDVU" />
+      
       <div>
         {songs.map((song) => {
-          return <li key={song.id}>{song.title}</li>;
+          return (
+          <div key={song.id} >
+          <li style = {{display: "inline-block"}}>{song.title} </li>
+          <YoutubeEmbed embedId={song.title}/>
+          <button onClick={()=>Delete(song.id)}>Delete</button>
+          </div>
+          );
         })}
       </div>
       <NewForm addSong={addSong} />
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 import { useState } from "react";
+
+// const host = "http://127.0.0.1:8000"
+const host = "https://pickend.herokuapp.com"
+
 const NewForm = ({ addSong }) => {
   const [url, setURL] = useState("");
 
   const handle = (e) => {
     e.preventDefault();
-    const address = "https://pickend.herokuapp.com/api/task-create/";
+    const address = host.concat("/api/task-create/")
     const data = { title: url, completed: false };
     axios
       .post(address, data)
